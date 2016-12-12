f64de42665dd82103bdcf5640df01534dd512aa1
Bao Pham <gbaopham@gmail.com>
2016-12-12 21:28:00 +0800

    Proper join form

--- universal/routes/Game/components/JoinForm/JoinForm.js
+++ universal/routes/Game/components/JoinForm/JoinForm.js
@@ -1,20 +1,74 @@
 import React, { PropTypes } from 'react'
+import {
+  Button,
+  Modal,
+  FormGroup,
+  ControlLabel,
+  FormControl
+} from 'react-bootstrap'
 
 export default class JoinForm extends React.Component {
   static propTypes = {
     onJoin: PropTypes.func.isRequired
   }
 
-  componentDidMount () {
-    const username = window.prompt('Enter a username to join this game')
-    if (username && username.trim()) {
-      this.props.onJoin(username.trim())
+  constructor(...args) {
+    super(...args)
+
+    this.state = {
+      username: ''
     }
+  }
+
+  getValidationState () {
+    const { username } = this.state
+
+    if (!username) {
+      return 'error'
+    }
+
+    return 'success'
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      username: e.target.value && e.target.value.trim()
+    })
+  }
+
+  join = () => {
+    const { username } = this.state
+    username && this.props.onJoin(username)
   }
 
   render () {
     return (
-      <div />
+      <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>Join Game</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <form>
+            <FormGroup
+              controlId='join-form'
+              validationState={this.getValidationState()}
+            >
+              <ControlLabel>Username</ControlLabel>
+              <FormControl
+                type='text'
+                value={this.state.username}
+                onChange={this.handleChange}
+              >
+              </FormControl>
+            </FormGroup>
+          </form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={this.join}>Join</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     )
   }
 }
