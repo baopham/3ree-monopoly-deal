fa69ec9a791d85f8a722a8d9b40b37d71bb31297
Bao Pham <gbaopham@gmail.com>
2016-12-26 12:03:51 +0800

    Table to list games

--- universal/routes/Games/components/GamesList/GamesList.js
+++ universal/routes/Games/components/GamesList/GamesList.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react'
 import { Link } from 'react-router'
+import { Table } from 'react-bootstrap'
 
 type Props = {
   games: Game[]
@@ -15,13 +16,24 @@ export default class GamesList extends React.Component {
     return (
       <div>
         <h1>Games</h1>
-        <ol>
-          {games.map((game, i) =>
-            <li key={i}>
-              <Link to={`/games/${game.id}`}>{game.id}</Link>
-            </li>
-          )}
-        </ol>
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Winner</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {games.map((game, i) =>
+              <tr key={i}>
+                <td><Link to={`/games/${game.id}`}>{game.name}</Link></td>
+                <td>{game.winner}</td>
+                <td>{game.createdAt}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
       </div>
     )
   }
