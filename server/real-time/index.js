2b9a2f8c85776af85a4b9c4fbdc0c70e9e2c0949
Bao Pham <gbaopham@gmail.com>
2017-01-02 20:45:50 +0800

    Setup websocket for SayNo

--- server/real-time/index.js
+++ server/real-time/index.js
@@ -1,9 +1,11 @@
 import GameService from '../services/GameService'
 import PlayerService from '../services/PlayerService'
 import GameHistoryService from '../services/GameHistoryService'
+import SayNoService from '../services/SayNoService'
 
 export default function setup (io) {
   GameService.liveUpdates(io)
   PlayerService.liveUpdates(io)
   GameHistoryService.liveUpdates(io)
+  SayNoService.liveUpdates(io)
 }
