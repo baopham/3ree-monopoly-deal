756d539a3f5b37c5e3c9a4689e188ca778a9c1c3
Bao Pham <gbaopham@gmail.com>
2017-01-02 20:43:32 +0800

    Add selectors

--- universal/routes/Game/modules/gameSelectors.js
+++ universal/routes/Game/modules/gameSelectors.js
@@ -1,8 +1,15 @@
 /* @flow */
+import { SAY_NO } from '../../../monopoly/cards'
 import { createSelector } from 'reselect'
 import type { CurrentGameState } from './currentGame'
+import type { PaymentState } from './payment'
+import type { CurrentPlayerCardsOnHandState } from './currentPlayerCardsOnHand'
+import type { SayNoState } from './sayNo'
 
 const getCurrentGame = (state) => state.currentGame
+const getPayment = (state) => state.payment
+const getCurrentPlayerCardsOnHand = (state) => state.currentPlayerCardsOnHand
+const getSayNoState = (state) => state.sayNo
 
 export const getCurrentPlayer = createSelector(
   [getCurrentGame],
@@ -36,5 +43,34 @@ export const getOtherPlayers = createSelector(
     return currentPlayer &&
       currentGame.game &&
       currentGame.game.players.filter(player => player.id !== currentPlayer.id)
+  }
+)
+
+export const isPayee = createSelector(
+  [getCurrentPlayer, getPayment],
+  (currentPlayer: Player, payment: PaymentState): boolean => {
+    return !!(currentPlayer && payment.amount && payment.payee === currentPlayer.username)
+  }
+)
+
+export const isPayer = createSelector(
+  [getCurrentPlayer, getPayment],
+  (currentPlayer: Player, payment: PaymentState): boolean => {
+    return !!(currentPlayer && payment.payers && payment.payers.includes(currentPlayer.username))
+  }
+)
+
+export const canSayNo = createSelector(
+  [getCurrentPlayerCardsOnHand],
+  (currentPlayerCardsOnHand: CurrentPlayerCardsOnHandState): boolean => {
+    const { cardsOnHand } = currentPlayerCardsOnHand
+    return cardsOnHand.includes(SAY_NO)
+  }
+)
+
+export const canRespondToASayNo = createSelector(
+  [canSayNo, getSayNoState, getCurrentPlayer],
+  (canSayNo: boolean, sayNo: SayNoState, currentPlayer: Player): boolean => {
+    return currentPlayer.username === sayNo.toUser && canSayNo
   }
 )
