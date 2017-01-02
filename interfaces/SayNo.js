11c7c5c9ba9c9be24c4bbd4a8e2899f40c644e4f
Bao Pham <gbaopham@gmail.com>
2017-01-02 20:43:14 +0800

    Add SayNo type

---
+++ interfaces/SayNo.js
@@ -0,0 +1,10 @@
+import sayNoCauses from '../universal/monopoly/sayNoCauses'
+import type { SayNoCause, SayNoCauseInfo } from '../universal/monopoly/sayNoCauses'
+
+type SayNo = {
+  fromUser: Username,
+  toUser: Username,
+  gameId: string,
+  cause: SayNoCause,
+  causeInfo: SayNoCauseInfo
+}
