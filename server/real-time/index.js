0b64254d1e1f1cbf907150975adc6cd633ca927f
Bao Pham <gbaopham@gmail.com>
2017-01-08 01:29:36 +0800

    Real time for CardRequest

--- server/real-time/index.js
+++ server/real-time/index.js
@@ -2,10 +2,12 @@ import GameService from '../services/GameService'
 import PlayerService from '../services/PlayerService'
 import GameHistoryService from '../services/GameHistoryService'
 import SayNoService from '../services/SayNoService'
+import CardRequestService from '../services/CardRequestService'
 
 export default function setup (io) {
   GameService.liveUpdates(io)
   PlayerService.liveUpdates(io)
   GameHistoryService.liveUpdates(io)
   SayNoService.liveUpdates(io)
+  CardRequestService.liveUpdates(io)
 }
